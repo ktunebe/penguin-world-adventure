16080524257e44ba21b85dc3d0139a084a365840
ktunebe <ktunebe@gmail.com>
2024-07-18 13:29:39 -0500

    added scripts, added plugins and loaders to webpack, added database functionality

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,20 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+import { precacheAndRoute } from 'workbox-precaching'
+import { registerRoute } from 'workbox-routing'
+import { StaleWhileRevalidate } from 'workbox-strategies'
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST)
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+const cacheName = 'static-resources'
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+)
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,50 @@
-import { openDB } from 'idb';
+import { openDB } from 'idb'
+
+const dbName = 'jate'
+const dbVersion = 1
+const objectStoreName = 'jate'
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB(dbName, 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains(objectStoreName)) {
+				console.log(`${dbName} database already exists`)
+				return
+			}
+			db.createObjectStore(objectStoreName, { 
+        keyPath: 'id', 
+        autoIncrement: true 
+      })
+			console.log(`${dbName} database created`)
+		},
+	})
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+	// Open db
+	const db = await openDB(dbName, dbVersion)
+	// Transaction
+	const tx = await db.transaction(objectStoreName, 'readwrite')
+	// Access store from tx
+	const store = await tx.objectStore(objectStoreName)
+	// Add content to db
+	const result = await store.add({
+    content
+	})
+	return result
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Open db
+  const db = await openDB(dbName, dbVersion)
+  // Transaction
+  const tx = await db.transaction(objectStoreName, 'readonly')
+  // Access store from tx
+  const store = await tx.objectStore(objectStoreName)
+  // Get all content
+  const results = await store.getAll()
+  return results
+}
 
-initdb();
+initdb()
